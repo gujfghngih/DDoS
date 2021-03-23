96ab8fbd49f30ea2f7fa26e7fcd7ac8fe6804280
gujfghngih <68965425+gujfghngih@users.noreply.github.com>
2021-03-23 22:16:36 +0100

    Update DDoS-1.0.py

--- DDoS-1.0.py
+++ DDoS-1.0.py
@@ -194,7 +194,7 @@ class slow:
 			except KeyboardInterrupt:
 				break
 				sys.exit(cprint('[+] Attack canceled by user','red'))
-		print + colored +('We\'ve sent '),'green') + colored(str(self.pkt_count),('cyan') + colored(' packets successfully. We\'re sleeping for '),('green') + colored(self.sleep ,'red') + colored(' seconds'),('green')
+		print ('We\'ve sent ')(str(self.pkt_count)(' packets successfully. We\'re sleeping for ')(self.sleep)(' seconds')
 		time.sleep(self.sleep)
 
 class Requester(Thread):
